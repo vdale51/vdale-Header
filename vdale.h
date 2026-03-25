f45a8850e1fa14c93218c01d64a3d884de23155f
Dale Webster <56497920+vdale51@users.noreply.github.com>
2026-03-25 05:28:18 -0700

    Update copyright year in vdale.h

--- vdale.h
+++ vdale.h
@@ -1,7 +1,7 @@
 /*
  * vdale.h
  *
- * Copyright 2024 Vdale <>
+ * Copyright 2026 Vdale <>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
